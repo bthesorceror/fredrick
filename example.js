59dac89efa9f890a4cc356dba67ca77717f41aaf
bthesorceror <bthesorceror@gmail.com>
2016-02-16 19:15:14 -0700

    Add extensions to example

--- example.js
+++ example.js
@@ -1,17 +1,38 @@
 'use strict';
 
-const Fredrick = require('.');
+const Fredrick = require('fredrick');
 
 let fredrick = new Fredrick('fredrick');
 
+fredrick.addExtension({
+  name: 'test1',
+  func: function() {}
+});
+
+fredrick.addExtension({
+  name: 'test2',
+  extensions: ['test1'],
+  func: function() {
+    this.test1();
+  }
+});
+
 fredrick.addPlugin({
   command: 'test',
-  usage: 'fredrick test [--prod]',
+  usage: 'fredrick test [--prod] [--error]',
   description: 'a test command',
+  extensions: ['test2'],
   func(fredrick, args, options) {
     if (options.prod) {
       fredrick.write('PRODUCTION!');
     }
+
+    if (options.error) {
+      fredrick.error('MY BAD!');
+    }
+
+    fredrick.test2();
+
     fredrick.write('TESTING');
     fredrick.exit(0);
   },
--- readme.md
+++ readme.md
@@ -13,10 +13,24 @@ const Fredrick = require('fredrick');
 
 let fredrick = new Fredrick('fredrick');
 
+fredrick.addExtension({
+  name: 'test1',
+  func: function() {}
+});
+
+fredrick.addExtension({
+  name: 'test2',
+  extensions: ['test1'],
+  func: function() {
+    this.test1();
+  }
+});
+
 fredrick.addPlugin({
   command: 'test',
   usage: 'fredrick test [--prod] [--error]',
   description: 'a test command',
+  extensions: ['test2'],
   func(fredrick, args, options) {
     if (options.prod) {
       fredrick.write('PRODUCTION!');
@@ -25,6 +39,8 @@ fredrick.addPlugin({
     if (options.error) {
       fredrick.error('MY BAD!');
     }
+
+    fredrick.test2();
 
     fredrick.write('TESTING');
     fredrick.exit(0);
